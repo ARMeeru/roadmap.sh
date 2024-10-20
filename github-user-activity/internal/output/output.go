e26cbd080f3e42102a59ef3610cbaeb0cc018f04
Asifur Rahaman Meeru <asif@skillsdb.com>
2024-10-20 20:27:24 +0600

    feat: Update comments for FetchGitHubActivity, DisplayActivity, and Execute functions

--- github-user-activity/internal/output/output.go
+++ github-user-activity/internal/output/output.go
@@ -6,7 +6,13 @@ import (
 	"github.com/ARMeeru/github-user-activity/internal/github"
 )
 
-// DisplayActivity displays the GitHub activity in a human-readable format
+// DisplayActivity prints a summary of recent GitHub activities to the console.
+// It takes a slice of GitHubEvent objects as input and categorizes the events
+// into different types such as PushEvent, IssuesEvent, and WatchEvent. If no
+// events are found, it prints a message indicating that there is no recent activity.
+//
+// Parameters:
+//   - events: A slice of GitHubEvent objects representing the user's recent GitHub activities.
 func DisplayActivity(events []github.GitHubEvent) {
 	if len(events) == 0 {
 		fmt.Println("No recent activity found.")
